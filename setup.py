3ad14ed82f071cc038a1209621cb42b81022df8b
Kriss Mikelsons <mikelsonsk@gmail.com>
2014-05-08 22:08:49 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -1,62 +1,36 @@
-#!/usr/bin/env python
+# coding: utf8
 
-import io
-import os
 import re
+import os.path
 
 
-# Prevent spurious errors during `python setup.py test`, a la
-# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
-try:
-    pass
-except ImportError:
-    pass
-
-from setuptools import setup, find_packages
-
-
-def read(*parts):
-    with io.open(os.path.join(os.path.dirname(__file__), *parts)) as f:
-        return f.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-def find_install_requires():
-    return [x.strip() for x in
-            read('requirements.txt').splitlines()
-            if x.strip() and not x.startswith('#')]
+ROOT = os.path.dirname(__file__)
+README = open(os.path.join(ROOT, 'README.md')).read()
+INIT_PY = open(os.path.join(ROOT, 'dustcleaner', '__init__.py')).read()
+VERSION = re.search("VERSION = '([^']+)'", INIT_PY).group(1)
 
 
 setup(
-    name='css-dust-cleaner',
-    version=find_version('cssdustcleaner/__init__.py'),
-    description='clears unused CSS selectors',
-    long_description=read('README.md'),
+    name='dustcleaner',
+    version=VERSION,
     author='Kriss Mikelsons',
     author_email='kriss@application.lv',
+    description=
+        'Tool that removes unused CSS selectors by searching through definied directories',
+    long_description=README,
+    url='http://github.com/mkriss/css-dust-cleaner',
     license='MIT',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+    packages=['dustcleaner'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
     ],
-    install_requires=find_install_requires(),
-    entry_points={'console_scripts': ['cssdustcleaner=cssdustcleaner.dustcleaner:main']},
-    url='http://github.com/mkriss/css-dust-cleaner'
+    **extra_kwargs
 )
